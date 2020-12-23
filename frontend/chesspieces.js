ba9593df8a53c8bb5449a46a6dd071fc58ed3ec8
Rickson2019 <56167026+Rickson2019@users.noreply.github.com>
2020-12-23 06:50:53 -0800

    implemented grid-layout

--- frontend/chesspieces.js
+++ frontend/chesspieces.js
@@ -13,7 +13,8 @@ var __extends = (this && this.__extends) || (function () {
     };
 })();
 //how many px when it's zoomed 100%
-var SIDE_LENGTH = 80;
+var SIDE_LENGTH = 90 / (getZoomedRatio() * 0.01);
+var SIDE_LENGTH_vw = 70 / (getZoomedRatio() * 0.01);
 // used in class Piece
 var PieceColor;
 (function (PieceColor) {
@@ -52,8 +53,10 @@ var Board = /** @class */ (function () {
         this.side_length = this.ratio * 0.01 * SIDE_LENGTH;
         //A.K.A points.
         this.intersections = new Array();
+        this.div_2d_array = new Array();
         for (var i = 0; i < 9; i++) {
             this.intersections[i] = [];
+            this.div_2d_array[i] = [];
             for (var j = 0; j < 10; j++) {
                 // 1 2 3 4 5 6 7 8 9
                 // 2
@@ -66,9 +69,16 @@ var Board = /** @class */ (function () {
                 // 9
                 // 10
                 this.intersections[i][j] = (new Point(i + 1, j + 1));
+                var grid_div = document.createElement('div');
+                grid_div.id = "grid_div_" + (i + 1) + "_" + (j + 1);
+                grid_div.className = 'className_grid_div';
+                $(grid_div).css('grid-column', i + 1);
+                $(grid_div).css('grid-row', j + 1);
+                this.div_2d_array[i][j] = grid_div;
+                $('#board').append(this.div_2d_array[i][j]);
             }
         }
-        this.render();
+        ;
     }
     Board.prototype.detectZoom = function () {
         var ratio = 0;
@@ -98,21 +108,27 @@ var Board = /** @class */ (function () {
         this.image = document.createElement("img");
         this.image.src = './img/antontw_chinese_chess_plate.svg';
         this.image.id = 'id_chessboard';
-        $('#board').append(this.image);
+        var board = $('#board');
+        // $(board).append(this.image);
+        $(board).css('height', 'fit-content');
         $("#id_chessboard").css("position", "fixed");
+        $("#id_chessboard").css("max-height", '100vh');
+        $("#id_chessboard").css("max-width", "80vw");
+        // $("#id_chessboard").css("width", "80vw");
+        // $("#id_chessboard").css("height", "80vh");
+        $("#id_chessboard").css("margin", "0");
+        $("#id_chessboard").css("padding", "0");
     };
     return Board;
 }());
 var Piece = /** @class */ (function () {
-    function Piece(piece_role, board, point, img_filepath, color, element) {
+    function Piece(piece_role, board, point, color) {
         this.board = board;
         this.piece_role = piece_role;
         this.selected = false;
         this.active = false;
-        this.img_filepath = img_filepath;
         this.color = color;
         this.point = point;
-        this.element = element;
         // this.cell = this.board.get_cell(x_coor, y_coor);
         // this.cell.set_piece(this);
         this.ratio = 100; //default ratio 100%
@@ -124,7 +140,8 @@ var Piece = /** @class */ (function () {
         // var image = $(`<img src='${this.img_filepath}' />`)
         var _a;
         var image = document.createElement("img");
-        image.src = this.img_filepath;
+        if (this.img_filepath)
+            image.src = this.img_filepath;
         image.className = 'classname_pieces';
         image.id = 'general';
         console.log(this.img_filepath);
@@ -145,14 +162,13 @@ var Piece = /** @class */ (function () {
             $(image).css('height', this.side_length);
             $(image).css('width', this.side_length);
             (_a = document.getElementById("board")) === null || _a === void 0 ? void 0 : _a.append(image);
+            $(this.board.div_2d_array[this.point.x_coor - 1][this.point.y_coor - 1]).append(image);
             console.log('x_coor');
             console.log(x_coor);
             console.log('y_coor');
             console.log(y_coor);
             console.log(y_coor * SIDE_LENGTH);
             console.log((y_coor * SIDE_LENGTH).toString());
-            console.log($(this.element));
-            console.log(this.element);
             var ele_style = { 'left': left, 'top': top_1 };
             console.log(ele_style);
             console.log((x_coor * SIDE_LENGTH).toString());
@@ -162,8 +178,8 @@ var Piece = /** @class */ (function () {
 }());
 var General = /** @class */ (function (_super) {
     __extends(General, _super);
-    function General(board, point, img_filepath, color, element) {
-        var _this = _super.call(this, PieceRole.General, board, point, img_filepath, color, element) || this;
+    function General(board, point, color) {
+        var _this = _super.call(this, PieceRole.General, board, point, color) || this;
         if (_this.color == PieceColor.red) {
             _this.img_filepath = './img/pieces/red-shuai.png';
             _this.point = _this.board.intersections[4][9];
@@ -189,87 +205,85 @@ var Player = /** @class */ (function () {
     return Player;
 }());
 var board = new Board();
+board.render();
 board.intersections;
 for (var i = 0; i < 9; i++) {
     for (var j = 0; j < 10; j++) {
         console.log("xcoor: " + board.intersections[i][j].x_coor + " \n                     ycoor: " + board.intersections[i][j].y_coor);
+        console.log(board.div_2d_array[i][j]);
     }
 }
 var div = document.createElement("div");
-var black_jiang = new General(board, new Point(0, 4), './img/pieces/black-jiang.png', PieceColor.black, div);
-var red_shuai = new General(board, new Point(9, 4), './img/pieces/red-shuai.png', PieceColor.red, div);
+var black_jiang = new General(board, new Point(0, 4), PieceColor.black);
+var red_shuai = new General(board, new Point(9, 4), PieceColor.red);
+// You'll have to render it after creating an object
+// If you do it in the constructor,
+// Async problems occur.
 black_jiang.render();
 red_shuai.render();
-$(".classname_pieces").css("position", "absolute");
-$(".classname_pieces").css("background", "red");
 console.log("resize");
+function getZoomedRatio() {
+    var ratio = 0;
+    // screen = window.screen,
+    // ua = navigator.userAgent.toLowerCase();
+    if (window.devicePixelRatio !== undefined) {
+        ratio = window.devicePixelRatio;
+    }
+    // ???? not working, why?
+    // else if (~ua.indexOf('msie')) {
+    //     if (screen.deviceXDPI && screen.logicalXDPI) {
+    //         ratio = screen.deviceXDPI / screen.logicalXDPI;
+    //     }
+    // }
+    else if (window.outerWidth !== undefined && window.innerWidth !== undefined) {
+        ratio = window.outerWidth / window.innerWidth;
+    }
+    if (ratio) {
+        ratio = Math.round(ratio * 100);
+    }
+    console.log('ratio');
+    console.log(ratio);
+    return ratio;
+}
+function getChessBoardSize() {
+    console.log('chessboard size:');
+    var chessboard = $('#id_chessboard');
+    console.log('chessboad width');
+    console.log(chessboard.css('width'));
+    console.log('chessboad height');
+    console.log(chessboard.css('height'));
+}
 $(window).resize(function () {
     if (screen.width == window.innerWidth) {
-        var ratio = 0;
-        // screen = window.screen,
-        // ua = navigator.userAgent.toLowerCase();
-        if (window.devicePixelRatio !== undefined) {
-            ratio = window.devicePixelRatio;
-        }
-        // ???? not working, why?
-        // else if (~ua.indexOf('msie')) {
-        //     if (screen.deviceXDPI && screen.logicalXDPI) {
-        //         ratio = screen.deviceXDPI / screen.logicalXDPI;
-        //     }
-        // }
-        else if (window.outerWidth !== undefined && window.innerWidth !== undefined) {
-            ratio = window.outerWidth / window.innerWidth;
-        }
-        if (ratio) {
-            ratio = Math.round(ratio * 100);
-        }
-        console.log('ratio');
-        console.log(ratio);
+        console.log("at exact 100%");
+        getZoomedRatio();
+        getChessBoardSize();
     }
     else if (screen.width > window.innerWidth) {
         console.log("you have zoomed in the page i.e more than 100%");
-        var ratio = 0;
-        // screen = window.screen,
-        // ua = navigator.userAgent.toLowerCase();
-        if (window.devicePixelRatio !== undefined) {
-            ratio = window.devicePixelRatio;
-        }
-        // ???? not working, why?
-        // else if (~ua.indexOf('msie')) {
-        //     if (screen.deviceXDPI && screen.logicalXDPI) {
-        //         ratio = screen.deviceXDPI / screen.logicalXDPI;
-        //     }
-        // }
-        else if (window.outerWidth !== undefined && window.innerWidth !== undefined) {
-            ratio = window.outerWidth / window.innerWidth;
-        }
-        if (ratio) {
-            ratio = Math.round(ratio * 100);
-        }
-        console.log('ratio');
-        console.log(ratio);
+        getZoomedRatio();
+        getChessBoardSize();
     }
     else {
         console.log("you have zoomed out i.e less than 100%");
-        var ratio = 0;
-        // screen = window.screen,
-        // ua = navigator.userAgent.toLowerCase();
-        if (window.devicePixelRatio !== undefined) {
-            ratio = window.devicePixelRatio;
-        }
-        // ???? not working, why?
-        // else if (~ua.indexOf('msie')) {
-        //     if (screen.deviceXDPI && screen.logicalXDPI) {
-        //         ratio = screen.deviceXDPI / screen.logicalXDPI;
-        //     }
-        // }
-        else if (window.outerWidth !== undefined && window.innerWidth !== undefined) {
-            ratio = window.outerWidth / window.innerWidth;
-        }
-        if (ratio) {
-            ratio = Math.round(ratio * 100);
-        }
-        console.log('ratio');
-        console.log(ratio);
+        getZoomedRatio();
+        getChessBoardSize();
     }
 });
+$("#board").css('margin', 0);
+$("#board").css('padding', 0);
+console.log("$('#id_chessboard').css('height')");
+console.log($('#id_chessboard').css('width'));
+console.log("$('#id_chessboard').css('height')");
+console.log($('#id_chessboard').css('width'));
+// $("#board").css('width',$("#id_chessboard").css('width'));
+// testing
+$('.className_grid_div').css('background-color', 'rgba(255,0,0,0.3)');
+// $('.className_grid_div').css('display','none')
+// $(".classname_pieces").css("background", 'red');
+// $(".classname_pieces").css("position", "absolute");
+var width = $('.className_grid_div').css('width', SIDE_LENGTH_vw);
+$('.className_grid_div').css('height', SIDE_LENGTH_vw);
+$('.className_grid_div').css('z-index', 10);
+$('.className_grid_div').css('margin', 0);
+console.log();
--- frontend/chesspieces.ts
+++ frontend/chesspieces.ts
@@ -1,8 +1,8 @@
 // import Log from "../src/Util";
 
 //how many px when it's zoomed 100%
-const SIDE_LENGTH: number = 80;
-
+const SIDE_LENGTH: number = 90 / (getZoomedRatio() * 0.01);
+var SIDE_LENGTH_vw: number = 70 / (getZoomedRatio() * 0.01);
 
 // used in class Piece
 enum PieceColor {
@@ -49,7 +49,7 @@ class Board {
     // this.xCoor = xCoor;   
 
     intersections: Array<Array<any>>;
-
+    div_2d_array: Array<Array<any>>;
 
 
     //how to get ratio : https://www.jianshu.com/p/6db40c482899
@@ -65,9 +65,11 @@ class Board {
 
         //A.K.A points.
         this.intersections = new Array<Array<any>>();
+        this.div_2d_array  = new Array<Array<any>>(); 
 
         for (let i = 0; i < 9; i++) {
             this.intersections[i] = [];
+            this.div_2d_array[i] = [];
             for (let j = 0; j < 10; j++) {
 
                 // 1 2 3 4 5 6 7 8 9
@@ -83,9 +85,23 @@ class Board {
                 // 10
                 this.intersections[i][j] = (new Point(i + 1, j + 1));
 
+                let grid_div = document.createElement('div');
+
+                grid_div.id = `grid_div_${i+1}_${j+1}`;
+                grid_div.className = 'className_grid_div'
+
+                $(grid_div).css('grid-column',i+1);
+                $(grid_div).css('grid-row',j+1);
+
+                this.div_2d_array[i][j] = grid_div;
+
+                $('#board').append(this.div_2d_array[i][j]);
+                
+
+
             }
         }
-        this.render();
+        ;
 
     }
 
@@ -120,12 +136,23 @@ class Board {
     }
 
     render() {
+
         this.image = document.createElement("img");
         this.image.src = './img/antontw_chinese_chess_plate.svg';
         this.image.id = 'id_chessboard';
 
-        $('#board').append(this.image);
+        let board = $('#board');
+
+        // $(board).append(this.image);
+        $(board).css('height','fit-content');
+        
         $("#id_chessboard").css("position", "fixed");
+        $("#id_chessboard").css("max-height", '100vh' );
+        $("#id_chessboard").css("max-width", "80vw");
+        // $("#id_chessboard").css("width", "80vw");
+        // $("#id_chessboard").css("height", "80vh");
+        $("#id_chessboard").css("margin", "0");
+        $("#id_chessboard").css("padding", "0");
 
 
     }
@@ -142,31 +169,30 @@ class Piece {
     piece_role: PieceRole;
     selected: boolean;
     active: boolean;
-    img_filepath: string;
+    img_filepath?: string;
     side_length: number;
     ratio: number;
 
     point?: Point;
 
     color: PieceColor;
-    element: HTMLElement;
+
 
 
 
     constructor(piece_role: PieceRole, board: Board,point:Point,
-        img_filepath: string, color: PieceColor, element: HTMLElement) {
+         color: PieceColor) {
 
         this.board = board;
         this.piece_role = piece_role;
         this.selected = false;
         this.active = false;
-        this.img_filepath = img_filepath;
+       
         this.color = color;
 
 
         this.point = point;
 
-        this.element = element;
         // this.cell = this.board.get_cell(x_coor, y_coor);
         // this.cell.set_piece(this);
 
@@ -182,7 +208,8 @@ class Piece {
         // var image = $(`<img src='${this.img_filepath}' />`)
 
         var image = document.createElement("img");
-        image.src = this.img_filepath;
+        if(this.img_filepath)
+            image.src = this.img_filepath;
 
         image.className = 'classname_pieces'
         image.id = 'general'
@@ -216,6 +243,7 @@ class Piece {
             $(image).css('width', this.side_length)
 
             document.getElementById("board")?.append(image)
+            $(this.board.div_2d_array[this.point.x_coor-1][this.point.y_coor-1]).append(image);
 
 
             console.log('x_coor')
@@ -224,8 +252,7 @@ class Piece {
             console.log(y_coor)
             console.log(y_coor * SIDE_LENGTH)
             console.log((y_coor * SIDE_LENGTH).toString())
-            console.log($(this.element))
-            console.log(this.element)
+  
 
 
             let ele_style = { 'left': left, 'top': top }
@@ -245,8 +272,8 @@ class Piece {
 
 class General extends Piece {
 
-    constructor(board: Board, point: Point, img_filepath: string, color: PieceColor, element: HTMLElement) {
-        super(PieceRole.General, board,point, img_filepath, color, element);
+    constructor(board: Board, point: Point, color: PieceColor,) {
+        super(PieceRole.General, board, point, color);
 
         if (this.color == PieceColor.red) {
             this.img_filepath = './img/pieces/red-shuai.png';
@@ -278,6 +305,7 @@ class Player {
 
 
 let board = new Board();
+board.render();
 
 
 board.intersections;
@@ -285,100 +313,118 @@ for (let i = 0; i < 9; i++) {
     for (let j = 0; j < 10; j++) {
         console.log(`xcoor: ${board.intersections[i][j].x_coor} 
                      ycoor: ${board.intersections[i][j].y_coor}`)
+        console.log(board.div_2d_array[i][j]);
     }
 }
 
 
 let div = document.createElement("div");
-let black_jiang = new General(board, new Point(0, 4), './img/pieces/black-jiang.png', PieceColor.black, div)
-let red_shuai = new General(board, new Point(9, 4), './img/pieces/red-shuai.png', PieceColor.red, div)
 
+let black_jiang = new General(board, new Point(0, 4),PieceColor.black)
+let red_shuai = new General(board, new Point(9, 4), PieceColor.red)
+
+
+// You'll have to render it after creating an object
+// If you do it in the constructor,
+// Async problems occur.
 black_jiang.render();
 red_shuai.render();
 
-$(".classname_pieces").css("position", "absolute");
-$(".classname_pieces").css("background", "red");
+
+
 
 console.log("resize")
+
+
+
+
+function getZoomedRatio() {
+    var ratio = 0;
+        // screen = window.screen,
+        // ua = navigator.userAgent.toLowerCase();
+        if (window.devicePixelRatio !== undefined) {
+            ratio = window.devicePixelRatio;
+        }
+
+        // ???? not working, why?
+
+        // else if (~ua.indexOf('msie')) {
+        //     if (screen.deviceXDPI && screen.logicalXDPI) {
+        //         ratio = screen.deviceXDPI / screen.logicalXDPI;
+        //     }
+        // }
+        else if (window.outerWidth !== undefined && window.innerWidth !== undefined) {
+            ratio = window.outerWidth / window.innerWidth;
+        }
+        if (ratio) {
+            ratio = Math.round(ratio * 100);
+        }
+
+        console.log('ratio')
+        console.log(ratio)
+        
+        return ratio;
+}
+
+function getChessBoardSize() {
+    console.log('chessboard size:')
+    let chessboard = $('#id_chessboard')
+
+    console.log( 'chessboad width')
+    console.log(chessboard.css('width'))
+
+    console.log( 'chessboad height')
+    console.log(chessboard.css('height'))
+}
+
+
+
+
+
 $(window).resize(function () {
     if (screen.width == window.innerWidth) {
-        var ratio = 0;
-        // screen = window.screen,
-        // ua = navigator.userAgent.toLowerCase();
-        if (window.devicePixelRatio !== undefined) {
-            ratio = window.devicePixelRatio;
-        }
-
-        // ???? not working, why?
-
-        // else if (~ua.indexOf('msie')) {
-        //     if (screen.deviceXDPI && screen.logicalXDPI) {
-        //         ratio = screen.deviceXDPI / screen.logicalXDPI;
-        //     }
-        // }
-        else if (window.outerWidth !== undefined && window.innerWidth !== undefined) {
-            ratio = window.outerWidth / window.innerWidth;
-        }
-        if (ratio) {
-            ratio = Math.round(ratio * 100);
-        }
-
-        console.log('ratio')
-        console.log(ratio)
-
+        console.log("at exact 100%");
+        getZoomedRatio()
+        getChessBoardSize()
+        
     } else if (screen.width > window.innerWidth) {
         console.log("you have zoomed in the page i.e more than 100%");
-
-        var ratio = 0;
-        // screen = window.screen,
-        // ua = navigator.userAgent.toLowerCase();
-        if (window.devicePixelRatio !== undefined) {
-            ratio = window.devicePixelRatio;
-        }
-
-        // ???? not working, why?
-
-        // else if (~ua.indexOf('msie')) {
-        //     if (screen.deviceXDPI && screen.logicalXDPI) {
-        //         ratio = screen.deviceXDPI / screen.logicalXDPI;
-        //     }
-        // }
-        else if (window.outerWidth !== undefined && window.innerWidth !== undefined) {
-            ratio = window.outerWidth / window.innerWidth;
-        }
-        if (ratio) {
-            ratio = Math.round(ratio * 100);
-        }
-
-        console.log('ratio')
-        console.log(ratio)
-
+        getZoomedRatio()
+        getChessBoardSize()
 
     } else {
         console.log("you have zoomed out i.e less than 100%")
-
-        var ratio = 0;
-        // screen = window.screen,
-        // ua = navigator.userAgent.toLowerCase();
-        if (window.devicePixelRatio !== undefined) {
-            ratio = window.devicePixelRatio;
-        }
-
-        // ???? not working, why?
-
-        // else if (~ua.indexOf('msie')) {
-        //     if (screen.deviceXDPI && screen.logicalXDPI) {
-        //         ratio = screen.deviceXDPI / screen.logicalXDPI;
-        //     }
-        // }
-        else if (window.outerWidth !== undefined && window.innerWidth !== undefined) {
-            ratio = window.outerWidth / window.innerWidth;
-        }
-        if (ratio) {
-            ratio = Math.round(ratio * 100);
-        }
-
-        console.log('ratio')
-        console.log(ratio)
+        getZoomedRatio()
+        getChessBoardSize()
+        
     }
 });
+
+
+$("#board").css('margin',0);
+$("#board").css('padding',0);
+
+
+console.log("$('#id_chessboard').css('height')")
+console.log($('#id_chessboard').css('width'))
+
+console.log("$('#id_chessboard').css('height')")
+console.log($('#id_chessboard').css('width'))   
+
+// $("#board").css('width',$("#id_chessboard").css('width'));
+// testing
+$('.className_grid_div').css('background-color','rgba(255,0,0,0.3)')
+// $('.className_grid_div').css('display','none')
+// $(".classname_pieces").css("background", 'red');
+// $(".classname_pieces").css("position", "absolute");
+
+let width = 
+$('.className_grid_div').css('width',SIDE_LENGTH_vw)
+$('.className_grid_div').css('height',SIDE_LENGTH_vw)
+$('.className_grid_div').css('z-index',10)
+$('.className_grid_div').css('margin',0)
+
+console.log()
+
+
+
